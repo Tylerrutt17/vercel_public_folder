8155703817c0357f9161f84fdc96e2837482e03c
Tyler Rutt <Tylerrutt17@gmail.com>
2024-09-08 17:37:03 -0400

    api

---
+++ src/pages/api/blogdir.js
@@ -0,0 +1,17 @@
+import { existsSync } from 'fs';
+import { resolve } from 'path';
+
+export default function handler(req, res) {
+    // Retrieve the filename from the query string
+    // const { filename } = req.query;
+  
+    // Define the path to the file in the public directory
+    const filePath = resolve(process.cwd(), 'public', "blog");
+  
+    // Check if the file exists
+    if (existsSync(filePath)) {
+      res.status(200).json({ message: `File exists:` });
+    } else {
+      res.status(404).json({ message: `File does not exist` });
+    }
+}
