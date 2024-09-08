68515dacab5b545b70e6478b77d16c350f74cfaf
Tyler Rutt <Tylerrutt17@gmail.com>
2024-09-08 17:39:44 -0400

    blog dir

--- src/pages/api/blogdir.js
+++ src/pages/api/blogdir.js
@@ -1,17 +1,21 @@
-import { existsSync } from 'fs';
+import { existsSync, promises as fsPromises } from 'fs';
 import { resolve } from 'path';
 
-export default function handler(req, res) {
-    // Retrieve the filename from the query string
-    // const { filename } = req.query;
-  
+export default async function handler(req, res) {
     // Define the path to the file in the public directory
     const filePath = resolve(process.cwd(), 'public', "blog", 'random.mdx');
-  
+
     // Check if the file exists
     if (existsSync(filePath)) {
-      res.status(200).json({ message: `File exists:` });
+        try {
+            // Read the file contents asynchronously
+            const fileContents = await fsPromises.readFile(filePath, 'utf8');
+            res.status(200).json({ message: `File exists:`, content: fileContents });
+        } catch (error) {
+            // Handle potential errors during file reading
+            res.status(500).json({ message: `Error reading file: ${error.message}` });
+        }
     } else {
-      res.status(404).json({ message: `File does not exist` });
+        res.status(404).json({ message: `File does not exist` });
     }
 }
